0382d68bf9e73d7e4b4843ac2cb5dab9cc25330b
Jake Alexander <imjakeal123@gmail.com>
2021-09-20 22:36:11 -0600

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -334,7 +334,7 @@ function init(data) {
     const shop = (prodList, tBill, lastProd) => {
         let totalBill = tBill;
         const prId = generateProductId();
-        let product = null; // Assign the value of product here
+        let product = (Object.is(lastProd, undefined)) ? lastProd : getProduct(prodList, prId); // Assign the value of product here
         let productDetails = null; // Assign the value of productDetails here
 
         rl.question(`You can buy - ${productDetails}.\n Do you want to buy this item <Y/N>? `.yellow, function (option) {
