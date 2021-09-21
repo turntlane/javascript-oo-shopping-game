c60dc3e10d41b2ef2e117d9bfc8d1d1130484958
Jake Alexander <imjakeal123@gmail.com>
2021-09-21 09:43:11 -0600

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -348,11 +348,16 @@ function init(data) {
                 console.log(`${player.name} you earned ${player.getCurrentScore()} points!`.bold);
                 if (player.score >= 500) {
                     // Define and set new property status in the player object here
+                    Object.defineProperty(player, 'status', {
+                        value: 'Shopping Master'
+                    })
                     exitWon();
                 } else {
                     let iCount = ++player.items;
                     // Make the Object.defineProperty() call here to set the value of items using the value of iCount
-                    
+                    Object.defineProperty(player, 'items', {
+                      value: iCount  
+                    })
                     if (player.items < 10) {
                         shop(prodList, totalBill);
                     } else {
