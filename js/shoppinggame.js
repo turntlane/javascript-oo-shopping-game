8ddb362f5b262cbe5b7ea6ad3ece71d62963d912
Jake Alexander <imjakeal123@gmail.com>
2021-09-21 10:30:44 -0600

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -337,7 +337,7 @@ function init(data) {
         let totalBill = tBill;
         const prId = generateProductId();
         let product = (Object.is(lastProd, undefined)) ? lastProd : getProduct(prodList, prId); // Assign the value of product here
-        let productDetails = product.getDetails(); // Assign the value of productDetails here
+        let productDetails = product.rating.getDetails(); // Assign the value of productDetails here
 
         rl.question(`You can buy - ${productDetails}.\n Do you want to buy this item <Y/N>? `.yellow, function (option) {
             const regexYes = new RegExp('y', 'i'); // Use the RegExp built-in object type here as appropriate
