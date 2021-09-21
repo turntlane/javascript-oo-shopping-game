5ada97dbe529eb2d5f1fe8edb8fab5ed80bbd727
Jake Alexander <imjakeal123@gmail.com>
2021-09-21 11:04:23 -0600

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -390,6 +390,7 @@ function init(data) {
                 // Call rating setter method of playerRating to set user entered rate value here
                 playerRating.rating = r
                 // Call Object.assign() method here to populate target
+                Object.assign({}, player, playerRating)
                 
                 console.log(`${target.name} you rated this game as ${target.rate}`.green);
                 console.log("Thank you for your valuable feedback.".blue);
